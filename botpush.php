8f3f8d9916d41f31a69dde24fb926e3c319847f4
etnwindlp <wind_lp@hotmail.com>
2021-03-18 13:17:57 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = 'y6A0qb6g3IYdOgP0qIMugXb/Dv8c5ra2RcABD9ET+EZwGh+7VWteIy6P0Vup2GWUO7lM/5M8BnuknzkmCmTz4/eRb41CkR+lQwSqrmorMX0KF1rqWJH9RGItzjHCOcPCGtBjfHMsFggZOy2SEjSPCwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'fa64dd89479baa81a6bb89d683a793d0';
+$channelSecret = 'b415b669077cab5abc79b66c1643ad49';
 
 $pushID = 'U544d2c17391a78adf8d4c18feff70866';
 
